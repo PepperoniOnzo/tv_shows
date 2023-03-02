de66b14536ba9372e1f8538749218e50ccb6a8ae
Pepperoni <radchukmaxym@gmail.com>
2023-03-03 00:01:02 +0200

    color fixes

--- lib/widgets/list_widgets/search_bar.dart
+++ lib/widgets/list_widgets/search_bar.dart
@@ -1,41 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:tv_shows/data/constants/app_colors.dart';
 
 import '../../views/app_view.dart';
 
-class SearchBar extends StatefulWidget {
+class SearchBar extends StatelessWidget {
   const SearchBar({super.key});
 
   @override
-  State<SearchBar> createState() => _SearchBarState();
-}
-
-class _SearchBarState extends State<SearchBar> {
-  late TextEditingController _textController;
-
-  @override
-  void initState() {
-    _textController = TextEditingController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _textController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: TextField(
-        controller: _textController,
-        onChanged: (value) {
-          context.read<AppView>().setFilmName(value);
-        },
-        decoration: const InputDecoration(
-            hintText: 'Search...', prefixIcon: Icon(Icons.search)),
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextField(
+          onChanged: (value) {
+            context.read<AppView>().setFilmName(value);
+          },
+          cursorColor: AppColors.textSecondary,
+          decoration: const InputDecoration(
+              hintText: 'Search...', prefixIcon: Icon(Icons.search)),
+        ),
       ),
     );
   }
--- lib/widgets/list_widgets/search_results.dart
+++ lib/widgets/list_widgets/search_results.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:tv_shows/data/constants/app_colors.dart';
 import 'package:tv_shows/views/app_view.dart';
 import 'package:tv_shows/widgets/list_widgets/films_list.dart';
 
@@ -9,11 +10,29 @@ class SearchResults extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: context.watch<AppView>().isLoading
-            ? const CircularProgressIndicator()
-            : context.watch<AppView>().error
-                ? Text(context.watch<AppView>().errorMessage,
-                    style: Theme.of(context).textTheme.headlineSmall)
-                : const FilmList());
+      child: AnimatedSwitcher(
+        duration: const Duration(milliseconds: 500),
+        child: _buildChild(context),
+        transitionBuilder: (child, animation) => FadeTransition(
+          opacity: animation,
+          child: child,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildChild(BuildContext context) {
+    final appView = context.watch<AppView>();
+
+    if (appView.isLoading) {
+      return const CircularProgressIndicator(color: AppColors.textSecondary);
+    } else if (appView.error) {
+      return Text(
+        appView.errorMessage,
+        style: Theme.of(context).textTheme.headlineSmall,
+      );
+    } else {
+      return const FilmList();
+    }
   }
 }
